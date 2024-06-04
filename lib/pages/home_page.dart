52ea70ac9b740d4b1f347baa90961ea553da15c3
Alessandro D'Amico <alessandro.damico@intre.it>
2024-06-04 19:35:29 +0200

    add FutureBuilder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,40 @@
-
+import 'package:books/network/rest_client.dart';
+import 'package:books/repositories/book_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
+
+import '../models/book.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final log = Logger('HomePage');
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    BookRepository bookRepository = BookRepository(restClient: RestClient(Dio()));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Books'),
       ),
-      body: const Center(child: Text("Ciao")),
+      body: FutureBuilder<List<Book>>(
+        future: bookRepository.getAll(),
+        builder: (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            log.info(snapshot.data.toString());
+            return const Center(child: Text("Ci sono dati"));
+          } else if (snapshot.hasError) {
+            return const Center(child: Text("Errore", style: TextStyle(color: Colors.red, fontSize: 50)));
+          }
+
+          return const Center(child: Text("Caso non gestito"));
+        },
+      ),
     );
   }
 }
